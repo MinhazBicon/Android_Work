4f80cebf28a1b2e0b2fa97484688e37a29976dac
MinhazBicon <minhazul.alam142@northsouth.edu>
2020-01-30 21:07:59 +0600

    Basic ratingbar android

---
+++ Ratingbar/app/src/main/java/com/example/ratingbar/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.ratingbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private RatingBar ratingBar;
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.txt);
+        ratingBar = findViewById(R.id.ratingID);
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                textView.setText("Value: "+rating);
+            }
+        });
+    }
+}
